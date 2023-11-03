69cf88dd24f43f07061c9e195fa521ab53167d3f
aftabnaik19 <aftabnaik1419@gmail.com>
2023-11-03 14:52:15 +0530

    added radix ui library and removed react+vite template

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <navbar> 
-      <div style={{display : 'inline-block'}}>Hello 1</div>
-      <div style={{display : 'inline-block'}}>Hello 2</div>
-      <div style={{display : 'inline-block'}}>Hello 3</div>
-      <div style={{display : 'inline-block'}}>Hello 4</div>
-      <div style={{display : 'inline-block'}}>Hello 5</div>
-      <div style={{display : 'inline-block'}}>Hello 5</div>
-      <div style={{display : 'inline-block'}}>Hello 5</div>
-      <div style={{display : 'inline-block'}}>Hello 5</div>
-      <div style={{display : 'inline-block'}}>Hello 5</div>
-
-    </navbar>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <navbar></navbar>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
