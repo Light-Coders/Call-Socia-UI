a9b523d3c08cab79f968f885e765b6ea54312ac7
Lone2005Shinobi <khawsemanthan246@gmail.com>
2023-11-19 20:58:41 +0530

    added search section

---
+++ src/Components/SearchSection.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import search from '../assets/search.jpeg'
+import Searchbar from './Searchbar'
+import SearchResult from './SearchResult'
+
+function SearchSection() {
+  const [results, setResults] = useState([])
+  const [query, setQuery] = useState("")
+
+  return (
+    // <div className="flex flex-wrap items-center justify-around bg-sky-300 p-10" >
+    // <img src={search} alt="" className='h-96 shadow-2xl '/>
+    // <div className='flex flex-col w-2/5 sm:w-2/5 md:justify-center sm:text-center justify-center gap-11'>
+    // <h1 className='text-6xl font-bold w-4/5 text-white '>Search your college mates!</h1>
+    // <div className='relative'>
+    // <Searchbar setResults = {setResults} setQuery={setQuery} query={query}/>
+    // <SearchResult results={results} query={query}/>
+    // </div>
+    // </div>
+    // </div>
+
+    <div className="flex flex-col md:flex-row items-center justify-around bg-sky-300 p-5 md:p-10">
+      <img src={search} alt="" className='h-64 md:h-96 shadow-2xl mb-5 md:mb-0'/>
+      <div className='flex flex-col w-full md:w-2/5 md:justify-center sm:text-center justify-center gap-5'>
+        <h1 className='text-4xl md:text-6xl font-bold text-white'>Search your college mates!</h1>
+        <div className='relative'>
+          <Searchbar setResults={setResults} setQuery={setQuery} query={query}/>
+          <SearchResult results={results} query={query}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SearchSection
---
+++ src/Components/Searchbar.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import data from '../data2.json'
+
+
+function Searchbar({setResults , setQuery , query}) {
+
+  const [users , setUsers] = useState([]);
+
+  useEffect(() => {
+    setUsers(data);
+  }, [])
+  
+  const getResult = (value)=>{
+      const result = users.filter(user => {
+      return (value && user && user['Student Name'] && user['Student Name'].toLowerCase().includes(query.toLowerCase()));
+    })
+      setResults(result);
+  }
+
+  const handleChange = (value)=>{
+    setQuery(value);
+    getResult(value)
+  }
+
+  return (
+    <>
+    <input type="text" placeholder='Search...' className='focus:outline-none w-3/5 h-11 p-4 rounded-full' value={query} onChange={(e)=>handleChange(e.target.value)}/>
+    </>
+  )
+}
+
+export default Searchbar
